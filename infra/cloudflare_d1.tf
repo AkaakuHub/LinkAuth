15261f5f34b3e8630c2494001ec97a053e13dde9
AkaakuHub <akaaku2021media01@gmail.com>
2026-05-15 00:58:27 +0900

    D1 read replication設定を明示する

--- infra/cloudflare_d1.tf
+++ infra/cloudflare_d1.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_d1_database" "auth" {
-  account_id = var.cloudflare_account_id
-  name       = local.d1_database_name
+  account_id       = var.cloudflare_account_id
+  name             = local.d1_database_name
+  read_replication = { mode = "disabled" }
 }
